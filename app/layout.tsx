a73381f3d0cc598cfe531bf0fd04b136971a4dd7
Debugger-ops <vivek9to5@gmail.com>
2025-03-30 00:19:07 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Navbar from './components/layout/Navbar';
 import Sidebar from './components/layout/Sidebar';
 
-const Layout = ({ children }) => (
-   <>
-       <Navbar />
-       <Sidebar />
-       <main>{children}</main>
-   </>
-);
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <Sidebar />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
 
 export default Layout;
